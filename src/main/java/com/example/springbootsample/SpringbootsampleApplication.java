83f3cbc572508f0013ecdc2eb63018c96385f8f9
kishorekadali21 <kishorekadali@21gmail.com>
2023-09-12 20:50:11 +0530

    first commit

---
+++ src/main/java/com/example/springbootsample/SpringbootsampleApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsampleApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+	
+
+}
---
+++ src/test/java/com/example/springbootsample/SpringbootsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
